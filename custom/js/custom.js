26a467230ff00e0cb42c927ca425818cc94f0406
Catherine Corbesier <ccorbesier@gmail.com>
2015-07-25 23:11:44 +0200

    Use icons for duration, quantity, moment, temperature

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -7,14 +7,21 @@ $(document).ready(function() {
     success: function(appartthes) {
       var json = $.parseJSON(appartthes);
       $.each(json, function() {
-        if (this.Enseigne == "Pairi Daiza"){
+        if(this.Enseigne == "Pairi Daiza"){
           var whoDesc = "de Pairi Daiza";
         }else if(this.Enseigne == "Palais des Thés"){
           var whoDesc = "du Palais des Thés";
         }else{
           var whoDesc = "de la Septième Tasse"; 
         }
-        $("#accordion").append('<div id="'+this.ID+'" class="panel panel-default '+this.varieteID+'" title="'+this.varieteID+'"><div class="panel-heading" role="tab" id="heading'+this.ID+'"><h4 class="panel-title"><a class="nom" data-toggle="collapse" data-parent="#accordion" href="#collapse'+this.ID+'" aria-expanded="true" aria-controls="collapse'+this.ID+'">' + this.Nom + '<span class="iconAccordion"></span></a></h4></div><div id="collapse'+this.ID+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading'+this.ID+'"><div class="panel-body"><strong>Variété : </strong> '+this.Variété+'<br /><strong>Origine :</strong> '+this.Origine+'<br /><strong>Température d\'infusion</strong> : '+this.Temp+'<br /><strong>Durée d\'infusion</strong> : '+this.Durée+'<br /><strong>Quantité par tasse :</strong> '+this.Quantité+'<br /><strong>Jour/Nuit ?</strong> '+this.jourNuit+'<br /><strong>Description '+whoDesc+' :</strong> '+this.Description+'<br /></div>');
+        if(this.jourNuit == "Jour"){
+          var jourNuitImg = "jour.png";
+        }else if (this.jourNuit == "Nuit"){
+          var jourNuitImg = "nuit.png";
+        }else{
+          var jourNuitImg = "jourNuit.png";
+        }
+        $("#accordion").append('<div id="'+this.ID+'" class="panel panel-default '+this.varieteID+'" title="'+this.varieteID+'"><div class="panel-heading" role="tab" id="heading'+this.ID+'"><h4 class="panel-title"><a class="nom" data-toggle="collapse" data-parent="#accordion" href="#collapse'+this.ID+'" aria-expanded="true" aria-controls="collapse'+this.ID+'">' + this.Nom + '<span class="iconAccordion"></span></a></h4></div><div id="collapse'+this.ID+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading'+this.ID+'"><div class="panel-body"><div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 leftSide"><strong>Variété : </strong> '+this.Variété+'<br /><strong>Origine :</strong> '+this.Origine+'</div><div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 rightSide"><div class="itemIcons"><img class="icons" src="custom/img/temp.png" alt="Température d\'infusion" /> '+this.Temp+'</div><div class="itemIcons"><img class="icons" src="custom/img/duree.png" alt="Durée d\'infusion" />'+this.Durée+'</div><div class="itemIcons"><img class="icons" src="custom/img/quantite.png" alt="Quantité" /> '+this.Quantité+'</div><div class="itemIcons"><img class="icons" src="custom/img/'+jourNuitImg+'" alt="'+this.jourNuit+'" /></div></div><div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><strong>Description '+whoDesc+' :</strong> '+this.Description+'</div></div>');
       });
     }
   });
