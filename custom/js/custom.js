c60d69843a1a80aa475f79f921a0f33b57b94925
Catherine Corbesier <ccorbesier@gmail.com>
2015-07-26 18:52:04 +0200

    Add possibility to filter by "jour/nuit ?"

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         }else{
           var jourNuitImg = "jourNuit.png";
         }
-        $("#accordion").append('<div id="'+this.ID+'" class="panel panel-default '+this.varieteID+'" title="'+this.varieteID+'"><div class="panel-heading" role="tab" id="heading'+this.ID+'"><h4 class="panel-title"><a class="nom" data-toggle="collapse" data-parent="#accordion" href="#collapse'+this.ID+'" aria-expanded="true" aria-controls="collapse'+this.ID+'">' + this.Nom + '<span class="iconAccordion"></span></a></h4></div><div id="collapse'+this.ID+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading'+this.ID+'"><div class="panel-body"><div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 leftSide"><strong>Variété : </strong> '+this.Variété+'<br /><strong>Origine :</strong> '+this.Origine+'</div><div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 rightSide"><div class="itemIcons"><img class="icons" src="custom/img/temp.png" alt="Température d\'infusion" /> '+this.Temp+'</div><div class="itemIcons"><img class="icons" src="custom/img/duree.png" alt="Durée d\'infusion" />'+this.Durée+'</div><div class="itemIcons"><img class="icons" src="custom/img/quantite-tasse.png" alt="Quantité par tasse" />'+this.Quantité+'</div><div class="itemIcons"><img class="icons" src="custom/img/quantite-bouilloire.png" alt="Quantité pour la bouilloire remplie au minimum" />'+this.quantitéBouilloire+'</div><div class="itemIcons"><img class="icons" src="custom/img/'+jourNuitImg+'" alt="'+this.jourNuit+'" /></div></div><div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><strong>Description '+whoDesc+' :</strong> '+this.Description+'</div></div>');
+        $("#accordion").append('<div id="'+this.ID+'" class="panel panel-default '+this.varieteID+'" title="'+this.varieteID+'"><div class="panel-heading" role="tab" id="heading'+this.ID+'"><h4 class="panel-title"><a class="nom" data-toggle="collapse" data-parent="#accordion" href="#collapse'+this.ID+'" aria-expanded="true" aria-controls="collapse'+this.ID+'">' + this.Nom + '<span class="iconAccordion"></span></a></h4></div><div id="collapse'+this.ID+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading'+this.ID+'"><div class="panel-body"><div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 leftSide"><strong>Variété : </strong> '+this.Variété+'<br /><strong>Origine :</strong> '+this.Origine+'</div><div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 rightSide"><div class="itemIcons"><img class="icons" src="custom/img/temp.png" alt="Température d\'infusion" /> '+this.Temp+'</div><div class="itemIcons"><img class="icons" src="custom/img/duree.png" alt="Durée d\'infusion" />'+this.Durée+'</div><div class="itemIcons"><img class="icons" src="custom/img/quantite-tasse.png" alt="Quantité par tasse" />'+this.Quantité+'</div><div class="itemIcons"><img class="icons" src="custom/img/quantite-bouilloire.png" alt="Quantité pour la bouilloire remplie au minimum" />'+this.quantitéBouilloire+'</div><div class="itemIcons"><img class="icons jourNuitClass" src="custom/img/'+jourNuitImg+'" alt="'+this.jourNuit+'" title="'+this.jourNuitID+'" /></div></div><div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><strong>Description '+whoDesc+' :</strong> '+this.Description+'</div></div>');
       });
     }
   });
@@ -32,11 +32,26 @@ $(document).ready(function() {
     $('#filter-type li a').click(function(e) {
       $('.panel').show();
       for (i=0; i<100; i++){
-        var menuID = $(this).attr('id');
+        var dropdownMenuID = $(this).attr('id');
         var panelTitle = $("#"+i).attr("title");
-        if(menuID != panelTitle){
+        if(dropdownMenuID != panelTitle){
 //          $($("#"+i).hasClass(panelTitle)).show();
           $('#'+i).hide();
+        }
+      }
+    });
+});
+
+/* Filters by "Jour/nuit ?" */
+$(document).ready(function() {
+    $('#filter-jourNuit li a').click(function(e) {
+      $('.panel').show();
+      for (i=0; i<100; i++){
+        var dropdownMenuID2 = $(this).attr('id');
+        var jourNuit = $($('.panel#'+i).find(".jourNuitClass")).attr('title');
+        if(dropdownMenuID2 != jourNuit){
+//          $($("#"+i).hasClass(panelTitle)).show();
+          $('.panel#'+i).hide();
         }
       }
     });
