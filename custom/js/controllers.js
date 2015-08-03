359bd6ae9d78efa8222c61b3aa06af31b4db7856
Catherine Corbesier <ccorbesier@gmail.com>
2015-08-03 11:41:27 +0200

    Reimplement jour/nuit icons + some UI/UX improvements

--- custom/js/controllers.js
+++ custom/js/controllers.js
@@ -3,12 +3,14 @@ theControllers.controller('SearchController', ['$scope', '$http', '$routeParams'
 	$http.get('custom/json/appart-thes.json').success(function(data) {
 		$scope.thes = data.thes.the;
 		$scope.theOrder = "name";
+        $scope.oneAtATime = true;
 	});
 }]);
 theControllers.controller('TypesController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
     $http.get('custom/json/appart-thes.json').success(function(data) {
 		$scope.thes = data.thes.the;
         $scope.thevarietes = {"infusions" : true, "oolong" : true, "rooiboos" : true, "thés blancs" : true, "thés jaunes" : true, "thés noirs" : true, "thés verts" : true, "non précisé" : true};
+        $scope.oneAtATime = true;
 	});
 
 }]);
