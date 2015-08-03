c94c7f4c43848882c5887207e134adc1e8c8e650
Catherine Corbesier <ccorbesier@gmail.com>
2015-08-03 16:01:56 +0200

    Add possibility to check/uncheck all

--- custom/js/controllers.js
+++ custom/js/controllers.js
@@ -9,8 +9,17 @@ theControllers.controller('SearchController', ['$scope', '$http', '$routeParams'
 theControllers.controller('TypesController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
     $http.get('custom/json/appart-thes.json').success(function(data) {
 		$scope.thes = data.thes.the;
-        $scope.thevarietes = {"infusions" : true, "oolong" : true, "rooiboos" : true, "thés blancs" : true, "thés jaunes" : true, "thés noirs" : true, "thés verts" : true, "non précisé" : true};
+        $scope.thevarietes = {"infusions" : false, "oolong" : false, "rooiboos" : false, "thés blancs" : false, "thés jaunes" : false, "thés noirs" : false, "thés verts" : false, "non précisé" : false};
         $scope.oneAtATime = true;
+        $scope.checkAll = function () {
+            if ($scope.selectedAll) {
+                $scope.selectedAll = true;
+                $scope.thevarietes = {"infusions" : true, "oolong" : true, "rooiboos" : true, "thés blancs" : true, "thés jaunes" : true, "thés noirs" : true, "thés verts" : true, "non précisé" : true};
+            } else {
+                $scope.selectedAll = false;
+                $scope.thevarietes = {"infusions" : false, "oolong" : false, "rooiboos" : false, "thés blancs" : false, "thés jaunes" : false, "thés noirs" : false, "thés verts" : false, "non précisé" : false};
+            }
+            
+        };
 	});
-
 }]);
